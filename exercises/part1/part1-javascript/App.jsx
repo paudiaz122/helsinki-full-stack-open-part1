b7871bf563910198471b38fff4caa5d74a742d53
Paula Diaz <paula.diaz@miracommerce.com>
2025-03-04 16:16:31 -0300

    part 1 exercises

---
+++ exercises/part1/part1-javascript/App.jsx
@@ -0,0 +1,63 @@
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part, i) => {
+        return <Part key={`${part.name}${i}`} partName={part.name} exercises={part.exercises} />
+      })}
+    </>
+  )
+}
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((total, part) => {
+    return total = total + part.exercises
+  }, 0)
+
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Part = ({ partName, exercises }) => {
+  return (
+    <p>
+      {partName} {exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
