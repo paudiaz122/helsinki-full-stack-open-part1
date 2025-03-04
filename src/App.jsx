6634e558d9c2ec8726ec97c77687b63d2e5533a4
Paula Diaz <paula.diaz@miracommerce.com>
2025-03-04 16:17:58 -0300

    part 1 exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,50 @@
-const Header = ({ course }) => {
-  return (
-    <h1>{course}</h1>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part, i) => {
-        return <Part key={`${part.name}${i}`} partName={part.name} exercises={part.exercises} />
-      })}
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((total, part) => {
-    return total = total + part.exercises
-  }, 0)
-
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
-
-const Part = ({ partName, exercises }) => {
-  return (
-    <p>
-      {partName} {exercises}
-    </p>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [selected, setSelected] = useState(0)
+  const [mostVoted, setMostVoted] = useState(0)
+  const MAX  = 8
+  const [votes, setVotes] = useState(Array(MAX).fill(0))
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const manageClick = () => {
+    const newVotes = [...votes]
+    newVotes[selected]++
+
+    setVotes((prevVotes) => {
+      const newVotes = [...prevVotes]
+      newVotes[selected]++
+
+      return newVotes
+    })
+
+    // The correct way of implementing this is with useEffect. I'm not using it as it hasn't been introduced in the course yet.
+    const maxIndex = newVotes.reduce((maxIdx, vote, idx, array) => 
+      vote > array[maxIdx] ? idx : maxIdx, 0
+    );
+    setMostVoted(maxIndex)
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={manageClick}>Vote</button>
+      <button onClick={() => {setSelected(Math.floor(Math.random() * MAX))}}>Next Anecdote</button>
+      <p>Has {votes[selected]} votes</p>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVoted]}</p>
+      <p>Has {votes[mostVoted]} votes</p>
     </div>
   )
 }
